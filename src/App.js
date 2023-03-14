39382d490ef322660f13c09754240d63d4df94c6
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-14 22:12:09 +0300

    Update
    Updated member registration form for useformik and components.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,10 @@
 import './App.css';
-import { Formik } from 'formik';
+import MemberRegistration from './components/MemberRegistration';
 
 function App() {
   return (
     <div className="App">
-      <h1>Membership Registration Form</h1>
-      <Formik
-        initialValues={{
-          firstName: 'Lorem',
-          lastName: 'Ipsum',
-          email: 'lorem@ipsum.com',
-          gender: 'male',
-          hobies: [],
-          country: "Turkey",
-        }}
-        onSubmit={ (values) => {
-          console.log(values);
-        }}
-      >
-        {
-          ({handleSubmit, handleChange, values}) => (
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="firstName">First Name</label>
-              <input name='firstName' value={values.firstName} onChange={handleChange}/>
-    
-              <label htmlFor="lastName">Last Name</label>
-              <input name='lastName' value={values.lastName} onChange={handleChange}/>
-    
-              <label htmlFor="email">Email</label>
-              <input name='email' value={values.email} onChange={handleChange}/>
-
-              <span>Male</span>
-              <input type="radio" name="gender" value="male" onChange={handleChange} checked={values.gender === "male"}/>
-              <span>Female</span>
-              <input type="radio" name="gender" value="female" onChange={handleChange} checked={values.gender === "female"}/>
-
-              <input type="checkbox" name="hobies" value="Travel" onChange={handleChange}/> <span>Travel</span>
-              <input type="checkbox" name="hobies" value="Photography" onChange={handleChange}/> <span>Photography</span>
-              <input type="checkbox" name="hobies" value="Cinema" onChange={handleChange}/> <span>Cinema</span>
-
-              <select name='country' value={values.country} onChange={handleChange}>
-                <option value="turkey">Turkey</option>
-                <option value="england">England</option>
-                <option value="germany">Germany</option>
-                <option value="russia">Russia</option>
-                <option value="abd">ABD</option>
-                <option value="india">India</option>
-              </select>
-    
-              <button type="submit">Submit</button>
-
-              <code>{JSON.stringify(values)}</code>
-            </form>
-          )
-        }
-      </Formik>
+        <MemberRegistration />
     </div>
   );
 }
---
+++ src/components/MemberRegistration.js
@@ -0,0 +1,56 @@
+import { useFormik } from 'formik';
+
+function MemberRegistration()  {
+    const {handleSubmit, handleChange, values} = useFormik({
+        initialValues: {
+          firstName: 'Lorem',
+          lastName: 'Ipsum',
+          email: 'lorem@ipsum.com',
+          gender: 'male',
+          hobies: [],
+          country: "Turkey",
+        },
+        onSubmit: values => {
+          console.log(values);
+        },
+    });
+    return (
+        <div>
+            <h1>Membership Registration Form</h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="firstName">First Name</label>
+              <input name='firstName' value={values.firstName} onChange={handleChange}/>
+    
+              <label htmlFor="lastName">Last Name</label>
+              <input name='lastName' value={values.lastName} onChange={handleChange}/>
+    
+              <label htmlFor="email">Email</label>
+              <input name='email' value={values.email} onChange={handleChange}/>
+
+              <span>Male</span>
+              <input type="radio" name="gender" value="male" onChange={handleChange} checked={values.gender === "male"}/>
+              <span>Female</span>
+              <input type="radio" name="gender" value="female" onChange={handleChange} checked={values.gender === "female"}/>
+
+              <input type="checkbox" name="hobies" value="Travel" onChange={handleChange}/> <span>Travel</span>
+              <input type="checkbox" name="hobies" value="Photography" onChange={handleChange}/> <span>Photography</span>
+              <input type="checkbox" name="hobies" value="Cinema" onChange={handleChange}/> <span>Cinema</span>
+
+              <select name='country' value={values.country} onChange={handleChange}>
+                <option value="turkey">Turkey</option>
+                <option value="england">England</option>
+                <option value="germany">Germany</option>
+                <option value="russia">Russia</option>
+                <option value="abd">ABD</option>
+                <option value="india">India</option>
+              </select>
+    
+              <button type="submit">Submit</button>
+
+              <code>{JSON.stringify(values)}</code>
+            </form>
+        </div>
+    )
+}
+
+export default MemberRegistration;
