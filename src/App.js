86e45d615aaeba13a0c3a6d665f7dd8aab0ed0bd
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-18 23:18:41 +0300

    Update
    Loaded questions page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import MemberRegistration from './components/MemberRegistration';
+import QuestionsContent from './components/QuestionsContent';
 
 function App() {
   return (
     <div className="App">
+        <QuestionsContent />
         <MemberRegistration />
     </div>
   );
---
+++ src/components/QuestionsContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function QuestionsContent() {
+    return (
+        <div className="questions__content">
+            <span>Frequently Asked Questions</span>
+            <p>Are those HTML templates absolutely free for any kind of usage?</p>
+        </div>
+    )
+}
+
+export default QuestionsContent;
