f999251bc5c68300f189a9f1f6f34878f2e7aa30
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-20 23:02:56 +0300

    Update
    Updated tab content

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -3,8 +3,29 @@ import React from "react";
 function PageContent() {
     return (
         <div className="questions__content">
-            <span>Frequently Asked Questions</span>
-            <p>Are those HTML templates absolutely free for any kind of usage?</p>
+            <div className="tabs">
+                <input type="radio" name="tabs" id="tabone" checked="checked" />
+                <label for="tabone">Tab One</label>
+                <div className="tab">
+                    <h1>Tab One Content</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            
+                <input type="radio" name="tabs" id="tabtwo" />
+                <label for="tabtwo">Tab Two</label>
+                <div className="tab">
+                    <h1>Tab Two Content</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            
+                <input type="radio" name="tabs" id="tabthree" />
+                <label for="tabthree">Tab Three</label>
+                <div className="tab">
+                    <h1>Tab Three Content</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+            </div>
         </div>
     )
 }
