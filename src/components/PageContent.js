5604e67783fc3619a8e8b0e78a4fad9a8cd48be8
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-21 19:30:04 +0300

    Update
    Updated page content

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -5,19 +5,19 @@ function PageContent() {
         <div className="page__content">
             <div className="tabs">
                 <input type="radio" className="tabs__radio" name="tabs-example" id="tab1" defaultChecked />
-                <label htmlFor="tab1" className="tabs__label">Tab #1</label>
+                <label htmlFor="tab1" className="tabs__label">Tab 1</label>
                 <div className="tabs__content">
-                    CONTENT for Tab #1
+                    <p>1-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum nunc in ex viverra interdum. Curabitur non lacus imperdiet, placerat nisi at, efficitur diam. Aliquam vulputate ex quis placerat ultricies. Aenean ex quam, pharetra et massa vitae, gravida sollicitudin tortor. Cras volutpat condimentum justo non aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vehicula sed sem non consequat. Vivamus sed justo vel turpis sagittis sodales. Phasellus fringilla urna quam, id commodo felis facilisis quis. Ut accumsan, massa quis aliquam blandit, nunc justo malesuada ex, eget pulvinar purus metus nec tellus. Sed sodales vitae velit at efficitur. Ut pretium libero vitae tristique pellentesque. Quisque ac elit tincidunt, efficitur felis eu, varius dui.</p>
                 </div>
                 <input type="radio" className="tabs__radio" name="tabs-example" id="tab2" />
-                <label htmlFor="tab2" className="tabs__label">Tab #2</label>
+                <label htmlFor="tab2" className="tabs__label">Tab 2</label>
                 <div className="tabs__content">
-                    CONTENT for Tab #2
+                    <p>2-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum nunc in ex viverra interdum. Curabitur non lacus imperdiet, placerat nisi at, efficitur diam. Aliquam vulputate ex quis placerat ultricies. Aenean ex quam, pharetra et massa vitae, gravida sollicitudin tortor. Cras volutpat condimentum justo non aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vehicula sed sem non consequat. Vivamus sed justo vel turpis sagittis sodales. Phasellus fringilla urna quam, id commodo felis facilisis quis. Ut accumsan, massa quis aliquam blandit, nunc justo malesuada ex, eget pulvinar purus metus nec tellus. Sed sodales vitae velit at efficitur. Ut pretium libero vitae tristique pellentesque. Quisque ac elit tincidunt, efficitur felis eu, varius dui.</p>
                 </div>
                 <input type="radio" className="tabs__radio" name="tabs-example" id="tab3" />
-                <label htmlFor="tab3" className="tabs__label">Tab #3</label>
+                <label htmlFor="tab3" className="tabs__label">Tab 3</label>
                 <div className="tabs__content">
-                    CONTENT for Tab #3
+                    <p>3-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum nunc in ex viverra interdum. Curabitur non lacus imperdiet, placerat nisi at, efficitur diam. Aliquam vulputate ex quis placerat ultricies. Aenean ex quam, pharetra et massa vitae, gravida sollicitudin tortor. Cras volutpat condimentum justo non aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vehicula sed sem non consequat. Vivamus sed justo vel turpis sagittis sodales. Phasellus fringilla urna quam, id commodo felis facilisis quis. Ut accumsan, massa quis aliquam blandit, nunc justo malesuada ex, eget pulvinar purus metus nec tellus. Sed sodales vitae velit at efficitur. Ut pretium libero vitae tristique pellentesque. Quisque ac elit tincidunt, efficitur felis eu, varius dui.</p>
                 </div>
             </div>
         </div>
