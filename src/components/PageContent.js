efacebcdaf029018c3f327da98461d03a756002e
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-21 19:15:00 +0300

    Update
    Updated page content

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -4,15 +4,20 @@ function PageContent() {
     return (
         <div className="page__content">
             <div className="tabs">
-                <input type="radio" className="tabs__radio" name="tabs-example" id="tab1" checked />
-                <label for="tab1" className="tabs__label">Tab #1</label>
+                <input type="radio" className="tabs__radio" name="tabs-example" id="tab1" defaultChecked />
+                <label htmlFor="tab1" className="tabs__label">Tab #1</label>
                 <div className="tabs__content">
                     CONTENT for Tab #1
                 </div>
                 <input type="radio" className="tabs__radio" name="tabs-example" id="tab2" />
-                <label for="tab2" className="tabs__label">Tab #2</label>
+                <label htmlFor="tab2" className="tabs__label">Tab #2</label>
                 <div className="tabs__content">
                     CONTENT for Tab #2
+                </div>
+                <input type="radio" className="tabs__radio" name="tabs-example" id="tab3" />
+                <label htmlFor="tab3" className="tabs__label">Tab #3</label>
+                <div className="tabs__content">
+                    CONTENT for Tab #3
                 </div>
             </div>
         </div>
