8777dbb627ba9b59e94f1779601426baa0a1d748
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-23 21:55:43 +0300

    Update
    Updated content

--- src/components/MemberRegistration.js
+++ src/components/MemberRegistration.js
@@ -18,41 +18,45 @@ function MemberRegistration()  {
         <div className='member__registration'>
             <h1>Membership Registration Form</h1>
             <form onSubmit={handleSubmit}>
-              <div>
-              <label htmlFor="firstName">First Name</label>
-              <input name='firstName' value={values.firstName} onChange={handleChange}/>
+              <div className='content'>
+                <label htmlFor="firstName">First Name</label>
+                <input name='firstName' value={values.firstName} onChange={handleChange}/>
               </div>
-              <div>
-              <label htmlFor="lastName">Last Name</label>
-              <input name='lastName' value={values.lastName} onChange={handleChange}/>
+              <div className='content'>
+                <label htmlFor="lastName">Last Name</label>
+                <input name='lastName' value={values.lastName} onChange={handleChange}/>
               </div>
-              <div>
-              <label htmlFor="email">Email</label>
-              <input name='email' value={values.email} onChange={handleChange}/>
+              <div className='content'>
+                <label htmlFor="email">Email</label>
+                <input name='email' value={values.email} onChange={handleChange}/>
               </div>
-              <div>
-              <span>Male</span>
-              <input type="radio" name="gender" value="male" onChange={handleChange} checked={values.gender === "male"}/>
-              <span>Female</span>
-              <input type="radio" name="gender" value="female" onChange={handleChange} checked={values.gender === "female"}/>
+              <div className='content radio__content'>
+                <div className='radio'>
+                  <span>Male</span>
+                  <input type="radio" name="gender" value="male" onChange={handleChange} checked={values.gender === "male"}/>
+                </div>
+                <div className='radio'>
+                  <span>Female</span>
+                  <input type="radio" name="gender" value="female" onChange={handleChange} checked={values.gender === "female"}/>
+                </div>
               </div>
-              <div>
-              <input type="checkbox" name="hobies" value="Travel" onChange={handleChange}/> <span>Travel</span>
-              <input type="checkbox" name="hobies" value="Photography" onChange={handleChange}/> <span>Photography</span>
-              <input type="checkbox" name="hobies" value="Cinema" onChange={handleChange}/> <span>Cinema</span>
+              <div className='content check__content'>
+                <input type="checkbox" name="hobies" value="Travel" onChange={handleChange}/> <span>Travel</span>
+                <input type="checkbox" name="hobies" value="Photography" onChange={handleChange}/> <span>Photography</span>
+                <input type="checkbox" name="hobies" value="Cinema" onChange={handleChange}/> <span>Cinema</span>
               </div>
-              <div>
-              <select name='country' value={values.country} onChange={handleChange}>
-                <option value="turkey">Turkey</option>
-                <option value="england">England</option>
-                <option value="germany">Germany</option>
-                <option value="russia">Russia</option>
-                <option value="abd">ABD</option>
-                <option value="india">India</option>
-              </select>
+              <div className='content select__content'>
+                <select name='country' value={values.country} onChange={handleChange}>
+                  <option value="turkey">Turkey</option>
+                  <option value="england">England</option>
+                  <option value="germany">Germany</option>
+                  <option value="russia">Russia</option>
+                  <option value="abd">ABD</option>
+                  <option value="india">India</option>
+                </select>
               </div>
-              <div>
-              <button type="submit">Submit</button>
+              <div className='button__content'>
+                <button type="submit">Submit</button>
               </div>
               <code>{JSON.stringify(values)}</code>
             </form>
--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -4,17 +4,17 @@ function PageContent() {
     return (
         <div className="page__content">
             <div className="tabs">
-                <input type="radio" className="tabs__radio" name="tabs-example" id="tab1" defaultChecked />
+                <input type="radio" className="tabs__radio" name="tabs-input" id="tab1" defaultChecked />
                 <label htmlFor="tab1" className="tabs__label">Tab 1</label>
                 <div className="tabs__content">
                     <p>1-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum nunc in ex viverra interdum. Curabitur non lacus imperdiet, placerat nisi at, efficitur diam. Aliquam vulputate ex quis placerat ultricies. Aenean ex quam, pharetra et massa vitae, gravida sollicitudin tortor. Cras volutpat condimentum justo non aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vehicula sed sem non consequat. Vivamus sed justo vel turpis sagittis sodales. Phasellus fringilla urna quam, id commodo felis facilisis quis. Ut accumsan, massa quis aliquam blandit, nunc justo malesuada ex, eget pulvinar purus metus nec tellus. Sed sodales vitae velit at efficitur. Ut pretium libero vitae tristique pellentesque. Quisque ac elit tincidunt, efficitur felis eu, varius dui.</p>
                 </div>
-                <input type="radio" className="tabs__radio" name="tabs-example" id="tab2" />
+                <input type="radio" className="tabs__radio" name="tabs-input" id="tab2" />
                 <label htmlFor="tab2" className="tabs__label">Tab 2</label>
                 <div className="tabs__content">
                     <p>2-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum nunc in ex viverra interdum. Curabitur non lacus imperdiet, placerat nisi at, efficitur diam. Aliquam vulputate ex quis placerat ultricies. Aenean ex quam, pharetra et massa vitae, gravida sollicitudin tortor. Cras volutpat condimentum justo non aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vehicula sed sem non consequat. Vivamus sed justo vel turpis sagittis sodales. Phasellus fringilla urna quam, id commodo felis facilisis quis. Ut accumsan, massa quis aliquam blandit, nunc justo malesuada ex, eget pulvinar purus metus nec tellus. Sed sodales vitae velit at efficitur. Ut pretium libero vitae tristique pellentesque. Quisque ac elit tincidunt, efficitur felis eu, varius dui.</p>
                 </div>
-                <input type="radio" className="tabs__radio" name="tabs-example" id="tab3" />
+                <input type="radio" className="tabs__radio" name="tabs-input" id="tab3" />
                 <label htmlFor="tab3" className="tabs__label">Tab 3</label>
                 <div className="tabs__content">
                     <p>3-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum nunc in ex viverra interdum. Curabitur non lacus imperdiet, placerat nisi at, efficitur diam. Aliquam vulputate ex quis placerat ultricies. Aenean ex quam, pharetra et massa vitae, gravida sollicitudin tortor. Cras volutpat condimentum justo non aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce vehicula sed sem non consequat. Vivamus sed justo vel turpis sagittis sodales. Phasellus fringilla urna quam, id commodo felis facilisis quis. Ut accumsan, massa quis aliquam blandit, nunc justo malesuada ex, eget pulvinar purus metus nec tellus. Sed sodales vitae velit at efficitur. Ut pretium libero vitae tristique pellentesque. Quisque ac elit tincidunt, efficitur felis eu, varius dui.</p>
